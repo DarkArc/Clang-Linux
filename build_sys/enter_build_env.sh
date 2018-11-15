6db88765a40675a7d972a1aea0f4d18627542bba
Wyatt Childers <wchilders@nearce.com>
2018-11-15 16:52:07 -0500

    Make the build system a bit more flexible, fixed several small issues, and switched to the us mirror for ubuntu packages

--- build_sys/enter_build_env.sh
+++ build_sys/enter_build_env.sh
@@ -1,3 +1,2 @@
-mkdir -p ../build &&\
 docker build -t clang_build_env:latest . &&\
-docker run --rm -it --user $UID -v $PWD/../clang:/build_env/clang -v $PWD/../llvm:/build_env/llvm -v $PWD/../build:/build_env/build --entrypoint="/start_shell.sh" clang_build_env:latest
+docker run --rm -it --user $UID:$UID --privileged=true -v $HOME/.bash_history:/bash_history -v $PWD/../files:/build_env/ --entrypoint="/start_shell.sh" clang_build_env:latest
--- build_sys/src/setup_clang_build.sh
+++ build_sys/src/setup_clang_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd build/
-cmake -G Ninja -DLLVM_USE_LINKER=lld ../llvm/
+cmake -G Ninja -DLLVM_USE_LINKER=lld -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../llvm/
 
