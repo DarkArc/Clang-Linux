15994d6be706aa0f532221805a3864596c3c53cd
Wyatt Childers <wchilders@nearce.com>
2020-07-23 15:04:21 -0400

    Use a different variable as UID is reserved on some systems

--- build_sys/enter_build_env.sh
+++ build_sys/enter_build_env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-UID="${UID:-$(id -u)}"
+LOCAL_UID="${UID:-$(id -u)}"
 
 docker build -t clang_build_env:latest . &&\
-docker run --rm -it --user $UID:$UID --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged=true -v $PWD/user_data/:/home/builduser/ -v $PWD/../files:/build_env/ --entrypoint="/build_sys_internal/shell/start.sh" clang_build_env:latest
+docker run --rm -it --user $LOCAL_UID:$LOCAL_UID --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged=true -v $PWD/user_data/:/home/builduser/ -v $PWD/../files:/build_env/ --entrypoint="/build_sys_internal/shell/start.sh" clang_build_env:latest
